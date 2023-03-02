ee4cb1be7e9fa3c628c7f70d6aecaa9acf0de12e
ChornyiYevhenii <gibridius16@gmail.com>
2023-03-02 15:40:50 +0200

    fix4

---
+++ src/components/App.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const AppWrapp = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 40;
+  color: #019901;
+  /* padding-bottom: 20px; */
+  width: 350px;
+  border: solid 2px orange;
+  border-radius: 5px;
+  margin: 20px auto;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; 
+`;
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SectionWrapp = styled.div`
+/* padding-bottom: 20px;
+border: solid 3px orange;
+border-radius: 5px; */
+  width: 350px;
+  div {
+    h2 {
+      text-align: center;
+    }
+  }
+`;

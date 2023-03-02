85412f7c201552b4f1b33402948ab0546fe739c4
ChornyiYevhenii <gibridius16@gmail.com>
2023-03-02 15:51:04 +0200

    fix5

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -8,10 +8,10 @@ export const AppWrapp = styled.div`
   flex-direction: column;
   font-size: 40;
   color: #019901;
-  /* padding-bottom: 20px; */
+  padding: 0 20px;
   width: 350px;
   border: solid 2px orange;
   border-radius: 5px;
-  margin: 20px auto;
+  margin: 30px auto 0;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; 
 `;
